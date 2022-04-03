05ed58678f6dc3925e1df40c0f2f327816d3252b
aba168 <alex.biscoveanu@yahoo.ca>
2022-04-03 11:56:08 -0700

    little bit of re-organization

--- src/script.js
+++ src/script.js
@@ -7,6 +7,11 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 const experience = new Experience(canvas, scene)
 
+// Music
+const pon = './PonDeReplay.mp3'
+const popcorn = './popcorn.mp3'
+experience.camera.vizualizer.initializeAudio(popcorn)
+
 // Test Object
 const geometry = new THREE.BoxGeometry(5, 5, 5)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
@@ -14,27 +19,18 @@ const mesh = new THREE.Mesh(geometry, material)
 mesh.position.set(0, 10, -10)
 scene.add(mesh)
 
-const planeGeometry = new THREE.PlaneGeometry(64, 64, 64, 64)
-const planeMaterial = new THREE.MeshNormalMaterial({ wireframe: true })
-const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
-planeMesh.rotateX(-Math.PI / 2)
-//scene.add(planeMesh)
 
-experience.camera.vizualizer.initializeAudio('./popcorn.mp3')
-
+// Update Frames
 const clock = new THREE.Clock();
 const tick = () => {
     let deltaTime = clock.getDelta();
 
     experience.update()
+    experience.camera.vizualizer.update(deltaTime)
     
     experience.controller.playerControls(experience.camera.controls, deltaTime)
-    
     experience.emitter.trigger('tick')
 
-    experience.camera.vizualizer.update(deltaTime)
-
-    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
